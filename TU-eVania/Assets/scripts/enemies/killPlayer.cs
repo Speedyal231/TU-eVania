7e818d62e7d618fcc65a1d7eb17863a79d714bb7
teomankerki <59364911+teomankerki@users.noreply.github.com>
2023-11-21 13:49:18 +0100

    enemies

---
+++ TU-eVania/Assets/scripts/enemies/killPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killPlayer : MonoBehaviour {
+	//public Animator animator;
+	public levelManager level_manager;
+
+	// Use this for initialization
+	void Start () {
+		level_manager = FindObjectOfType<levelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player")
+        {
+			//animator.SetTrigger("damageTAKEN");
+			level_manager.RespawnPlayer();
+        } 
+    }
+}
---
+++ TU-eVania/Assets/scripts/level/cameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+	public tempPlayerController player;
+
+	public bool isFollowing;
+
+	public float xOffset;
+
+	public float yOffest;
+
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<tempPlayerController>();
+
+		isFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFollowing)
+        {
+			transform.position = new Vector3(player.transform.position.x+ xOffset, player.transform.position.y+ yOffest, transform.position.z);
+        }
+
+	}
+}
