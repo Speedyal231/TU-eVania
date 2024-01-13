a9735543884722e77ca0975dd775bc301f7f426e
teomankerki <59364911+teomankerki@users.noreply.github.com>
2024-01-13 17:02:09 +0100

    enemy finalized

--- TueVania/Assets/scripts/teomanScripts/enemies/pEenemy/enemyPatrol.cs
+++ TueVania/Assets/scripts/teomanScripts/enemies/pEenemy/enemyPatrol.cs
@@ -26,22 +26,16 @@ public class enemyPatrol : MonoBehaviour {
 		walled = Physics2D.OverlapCircle(wallCheck.position, wallCheckradius, whatIsWall);
 		atEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckradius, whatIsWall);
 
-		if (walled || !atEdge)
-        {
+		if (atEdge || walled)
+		{
 			moveRight = !moveRight;
-        }
-
-		if (moveRight)
-        {
-			transform.localScale = new Vector3(-1f, 1f, 1f);
-			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
 		}
-        else
-        {
-			transform.localScale = new Vector3(1f, 1f, 1f);
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		float moveDirection = moveRight ? 1f : -1f;
+
+		transform.localScale = new Vector3(moveDirection, 1f, 1f);
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed * moveDirection, GetComponent<Rigidbody2D>().velocity.y);
+			
 		}
-		
-	}
 }
