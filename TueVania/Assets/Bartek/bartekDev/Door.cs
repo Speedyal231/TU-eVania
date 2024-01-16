9799e27b95243bf621bdd003d4f7d6614068f627
teomankerki <59364911+teomankerki@users.noreply.github.com>
2024-01-16 15:02:38 +0100

    Doors added

---
+++ TueVania/Assets/Bartek/bartekDev/Door.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Interactable
+{
+    [Header("The door on the other side")]
+    [SerializeField] GameObject otherSideDoor;
+
+    // Walk through the door
+    public override void Interact(GameObject player)
+    {
+        Vector2 relative = player.transform.position - this.transform.position;
+        Vector2 exit = otherSideDoor.transform.position;
+        Vector2 final = relative + exit;
+        player.transform.position = final;
+    }
+
+    public override void ExtraInteract(GameObject player)
+    {
+        // empty on purpose
+    }
+
+}
