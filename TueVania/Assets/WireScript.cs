efd3f7f597aaa1a2d0e322394bdab0260ad25125
Bora <mbkaplan7@gmail.com>
2024-01-17 15:52:52 +0100

    minigame lastVerion

--- TueVania/Assets/WireScript.cs
+++ TueVania/Assets/WireScript.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class WireScript : MonoBehaviour
 {
+    //public List<Transform> TerminalLists;
     public Transform EndWire;
+    public Transform EndWire2;
+    public Transform EndWire3;
+    public Transform EndWire4;
     public LineRenderer Line;
     bool Dragging = false;
+    bool isFull;
+    public bool booleanValue; 
     Vector3 OriginalPosition;
     Vector3 positionDifference;
-    bool Connected = false;
+    List<bool> Connected; // List to keep track of connected states for each end wire
     static List<WireScript> connectedWires = new List<WireScript>();
 
     void Start()
     {
         OriginalPosition = transform.position;
+        InitializeConnections();
+    }
+
+    void InitializeConnections()
+    {
+        Connected = new List<bool> { false, false, false, false }; 
     }
 
     void Update()
@@ -26,21 +38,56 @@ public class WireScript : MonoBehaviour
             convertedMousePosition.z = 0;
             SetPosition(convertedMousePosition);
 
-            Vector3 endWireDifference = convertedMousePosition - EndWire.position;
-            if (endWireDifference.magnitude < .5f)
+            // Check each end wire for proximity
+            for (int i = 0; i < Connected.Count; i++)
             {
-                SetPosition(EndWire.position);
-                Dragging = false;
-                Connected = true;
-                connectedWires.Add(this);
+                Transform currentEndWire;
+                //Transform currentEndWire = (i == 0) ? EndWire : EndWire2;
+                if(i == 0)
+                {
+                    currentEndWire = EndWire;
+                } 
+                else if(i == 1)
+                {
+                    currentEndWire = EndWire2;
+                }
+                else if(i == 2)
+                {
+                    currentEndWire = EndWire3;
+                }
+                else
+                {
+                    currentEndWire = EndWire4;
+                }
+                Vector3 endWireDifference = convertedMousePosition - currentEndWire.position;
+
+                if (!Connected[i] && endWireDifference.magnitude < 0.5f)
+                {
+                    isFull = true;
+                    SetPosition(currentEndWire.position);
+                    Dragging = false;
+                    Connected[i] = true;
+                    connectedWires.Add(this);
+                    //LogConnection(true, i); // Log the connection
+                    break; // Break the loop after the first connection
+                }
+                else if (Connected[i] && endWireDifference.magnitude >= 0.5f)
+                {
+                    Connected[i] = false;
+                    connectedWires.Remove(this);
+                    //LogConnection(false, i); // Log the disconnection
+                    break; // Break the loop after the first disconnection
+                }
             }
         }
-        else if (Connected && Input.GetMouseButtonDown(0)) 
+        else if (Connected.Contains(false) && Input.GetMouseButtonDown(0))
         {
             Vector3 mousePosition = Input.mousePosition;
             Vector3 convertedMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
             convertedMousePosition.z = 0;
-            if (Vector3.Distance(convertedMousePosition, transform.position) < .5f)
+
+            // Check if the mouse is close to any wire's position for initiating dragging
+            if (Vector3.Distance(convertedMousePosition, transform.position) < 0.7f)
             {
                 Dragging = true;
             }
@@ -52,31 +99,41 @@ public class WireScript : MonoBehaviour
         transform.position = pPosition;
 
         positionDifference = pPosition - Line.transform.position;
-        Line.SetPosition(2, positionDifference - new Vector3(.5f, 0, 0));
-        Line.SetPosition(3, positionDifference - new Vector3(.15f, 0, 0));
+        Line.SetPosition(2, positionDifference - new Vector3(0.5f, 0, 0));
+        Line.SetPosition(3, positionDifference - new Vector3(0.15f, 0, 0));
     }
+
+//     void LogConnection(bool isConnected, int inputIndex)
+// {
+//     string connectionStatus = isConnected ? "connected" : "disconnected";
+//     string inputName = (inputIndex == 0) ? "EndWire" : "EndWire2";
+//     Debug.Log($"Wire {inputName} {connectionStatus}");
+
+//     // Check if both wires are connected
+//     if (connectedWires.Count == 2)
+//     {
+//         Debug.Log($"Wire 1 connected to {connectedWires[0].gameObject.name}");
+//         Debug.Log($"Wire 2 connected to {connectedWires[1].gameObject.name}");
+//     }
+// }
+
 
     void OnCollisionEnter2D(Collision2D col) 
     {
-        if (col.gameObject.name == "And") 
+        if (col.gameObject.name == "Input1AND") 
         {
-            Connected = true;
-            Dragging = false;
-            connectedWires.Add(this);
+            //
         }
     }
 
     void OnMouseDown()
     {
-        if (!Connected)
+        if (Connected.Contains(false))
         {
+            // If any end wire is not connected, initiate dragging
             Dragging = true;
-            connectedWires.Clear();
+            connectedWires.Clear(); 
             connectedWires.Add(this);
-        }
-        else if (Connected && !connectedWires.Contains(this))
-        {
-            Dragging = false;
         }
     }
 
@@ -84,9 +141,29 @@ public class WireScript : MonoBehaviour
     {
         Dragging = false;
 
-        if (!Connected)
+        if (Connected.Contains(false))
         {
-            // SetPosition(OriginalPosition);
+            //SetPosition(OriginalPosition);
         }
+    }
+
+    void makeFull(){
+        isFull = true;
+    }
+
+    public bool getBooleanValue(){
+        return booleanValue;
+    }
+
+    public void setBooleanValue(bool newVal){
+        booleanValue = newVal;
+    }
+
+    public bool getDrag(){
+        return Dragging;
+    }
+
+    public void setDrag(bool newVal){
+        Dragging = newVal;
     }
 }
